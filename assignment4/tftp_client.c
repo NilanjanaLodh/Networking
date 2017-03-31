659c8c83a9ae7c96a3d5f8cc3a978943ad2d11b1
NilanjanaLodh <nilanjanalodh@gmail.com>
2017-03-31 14:38:53 +0530

    Fixed recv_file().. works fine now

--- assignment4/tftp_client.c
+++ assignment4/tftp_client.c
@@ -6,6 +6,11 @@
 #include <stdlib.h> //exit(0);
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
  
 
@@ -135,7 +140,7 @@ void send_file(char *hostname ,char *filename)
 void recv_file(char *hostname ,char *filename)
 {
 	FILE *fp;
-	fp = fopen(filename, "a");
+	fp = fopen(filename, "w");
 	int filename_len = strlen(filename);
 
 	/// first send a RRQ to port 69
@@ -158,19 +163,25 @@ void recv_file(char *hostname ,char *filename)
     int blocknum=1;
     struct sockaddr_in reply_addr ;
     int addrlen= sizeof(reply_addr);
+
     while(1)
     {
+    	addrlen= sizeof(reply_addr);
     	bzero(buf,516);
-    	if (recvfrom(sockfd, buf, 516, 0, (struct sockaddr *) &reply_addr, &addrlen) == -1)
+    	n = recvfrom(sockfd, buf, 516, 0, (struct sockaddr *) &reply_addr, &addrlen);
+    	
+    	if (n == -1)
 	    {
 	        error("recvfrom()");
 	    }
-
+	    n -= 4;
 	    if(buf[1]==ERR)
-	    	error("transfer failure");
+	    	error("Server transfer failure");
 	     
-	    printf("received data : %s\n",buf+4);
-	    n= fwrite(buf+4,1,512,fp);
+	    //printf("Received data : %s\n",buf+4);
+
+	    fwrite(&buf[4],1,n,fp);
+	    printf("Received block of size n = %d\n", n);
 
 	    bzero(message, BUFLEN);
 		message[0]=0x0;
@@ -178,7 +189,7 @@ void recv_file(char *hostname ,char *filename)
 		message[2]=blocknum>>8;
 		message[3]=blocknum%(0xff+1);
 
-		if (sendto(sockfd, message, BUFLEN , 0 , (struct sockaddr *) &reply_addr, addrlen)==-1)
+		if (sendto(sockfd, message, 4 , 0 , (struct sockaddr *) &reply_addr, addrlen)==-1)
 	    {
 	            error("sendto()");
 	    }    	
@@ -191,11 +202,9 @@ void recv_file(char *hostname ,char *filename)
 	    }
 
     }
+
     fclose(fp);
     printf("Transfer complete.\n");
-
-
-
 
 }
 int main(int argc , char **argv)
